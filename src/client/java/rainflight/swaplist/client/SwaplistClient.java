f2a331f2d51e16c552c8e009a0b8bc364a40a005
Rainflight <rainflight24@gmail.com>
2026-02-12 23:16:26 -0500

    template mod

---
+++ src/client/java/rainflight/swaplist/client/SwaplistClient.java
@@ -0,0 +1,10 @@
+package rainflight.swaplist.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SwaplistClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/rainflight/swaplist/Swaplist.java
@@ -0,0 +1,10 @@
+package rainflight.swaplist;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Swaplist implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
